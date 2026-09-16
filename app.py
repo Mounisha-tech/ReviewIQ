558ad1659c69b450395c810246030d47c3e0e958
Mounisha-tech <mounisha.v.21@gmail.com>
2026-09-16 17:17:34 +0530

    Set up ReviewIQ project foundation

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+st.set_page_config(
+    page_title="ReviewIQ",
+    page_icon="✦",
+    layout="wide"
+)
+st.title("ReviewIQ")
+st.write("E-commerce Product Intelligence Platform")
+
+st.success("ReviewIQ is running!")
